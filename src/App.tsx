f3fe9ba12eedb4a781d6680f13fa66f9008e6342
j-artur <jartur.dev@gmail.com>
2022-07-23 23:04:28 -0300

    Show steps while executing

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { createStore } from 'solid-js/store'
 const set = <T extends unknown>(arr: T[], idx: number, t: T | ((t: T) => T)) =>
   arr.map((e, i) => (i === idx ? (typeof t === 'function' ? (t as (t: T) => T)(e) : t) : e))
 
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
+
 type Pos = [x: number, y: number]
 type PosStr = `${number}:${number}`
 const pos = ([x, y]: Pos): PosStr => `${x}:${y}`
 
 enum Tile {
   Empty = 'Empty',
+  Visited = 'Visited',
   Wall = 'Wall',
   Player = 'Player',
   Goal = 'Goal',
@@ -22,6 +25,7 @@ type Item = 'Unset' | 'Holding' | ['Set', Pos]
 
 const colors: Record<Tile, string> = {
   Empty: '#eee',
+  Visited: '#66d',
   Wall: '#333',
   Player: '#f00',
   Goal: '#0f0',
@@ -40,50 +44,72 @@ const neighbors = ([x, y]: Pos): Pos[] =>
   ])
 
 const [state, setState] = createStore({ width: 16, height: 16, tiles: [] as Tile[][] })
+
 const [width, setWidth] = createSignal(16)
 const [height, setHeight] = createSignal(16)
+
+const [isRunning, setRunning] = createSignal(false)
+const [visited, setVisited] = createSignal(new Set<PosStr>())
+const [steps, setSteps] = createSignal<Pos[]>([])
+
+const visit = (p: Pos) => {
+  if (visited().has(pos(p))) return
+  setVisited(v => v.add(pos(p)))
+  if (getTile(p) !== Tile.Goal) setTile(p, Tile.Visited)
+}
+
+const step = (p: Pos) => {
+  setSteps(s => [...s, p])
+  if (getTile(p) !== Tile.Goal) setTile(p, Tile.Path)
+}
+
 const [player, setPlayer] = createSignal<Item>('Unset')
 const [goal, setGoal] = createSignal<Item>('Unset')
+const [isUsingGrid, setUsingGrid] = createSignal(false)
+const [fps, setFps] = createSignal(100)
+
 const emptyTiles = () => new Array(height()).fill(new Array(width()).fill(Tile.Empty)) as Tile[][]
 const getTile = ([x, y]: Pos) => state.tiles[y]?.[x] ?? Tile.Wall
-const setTile = ([x, y]: Pos, t: Tile) =>
-  setState('tiles', set(state.tiles, y, set(state.tiles[y]!, x, t)))
-const [isGridUsed, setGridUsed] = createSignal(false)
+const setTile = ([x, y]: Pos, t: Tile) => setState('tiles', g => set(g, y, l => set(l, x, t)))
+
 const resetGrid = () => {
   setState({ width: width(), height: height(), tiles: emptyTiles() })
-  setGridUsed(false)
+  setUsingGrid(false)
   if (player()[0] === 'Set') setTile((player() as ['Set', Pos])[1], Tile.Player)
   if (goal()[0] === 'Set') setTile((goal() as ['Set', Pos])[1], Tile.Goal)
 }
 
 const allowed = (p: Pos): boolean => getTile(p) !== Tile.Wall && getTile(p) !== Tile.Player
 
-const findPath = (start: Pos): Pos[] => {
-  const visited: Set<PosStr> = new Set()
+const findPath = async (start: Pos): Promise<boolean> => {
+  setVisited(new Set<PosStr>())
+  setSteps([])
 
-  const findPathHelp = (start: Pos, steps: Pos[] = []): Pos[] => {
-    if (getTile(start) === Tile.Goal) return [...steps, start]
-    const moves = neighbors(start).filter(p => allowed(p) && !visited.has(pos(p)))
-    for (const p of moves) visited.add(pos(p))
+  const findPathHelp = async (start: Pos): Promise<boolean> => {
+    if (getTile(start) === Tile.Goal) return true
+    const moves = neighbors(start).filter(p => allowed(p) && !visited().has(pos(p)))
 
-    return moves.reduce((s, p) => {
-      const found = findPathHelp(p, steps)
-      return found.length ? [p, ...found] : s
-    }, [] as Pos[])
+    await sleep(1000 / fps())
+    for (const p of moves) visit(p)
+
+    for (const p of moves) {
+      if (await findPathHelp(p)) {
+        await sleep(1000 / fps())
+        step(p)
+        return true
+      }
+    }
+
+    return false
   }
 
-  return findPathHelp(start, [])
+  return findPathHelp(start)
 }
 
 const App: Component = () => {
   resetGrid()
 
-  let gridRef: HTMLDivElement = null!
-  let playerRef: HTMLDivElement = null!
-  let goalRef: HTMLDivElement = null!
-
-  const [isRunning, setRunning] = createSignal(false)
-  const [sps, setSps] = createSignal(25)
+  let gridRef: HTMLDivElement | null = null
 
   const handleMouse = (e: MouseEvent & { currentTarget: HTMLDivElement; target: Element }) => {
     e.preventDefault()
@@ -149,10 +175,10 @@ const App: Component = () => {
             Steps per Second:{' '}
             <input
               type="number"
-              value={sps()}
+              value={fps()}
               onInput={e => {
                 const sps = parseInt(e.currentTarget.value, 10)
-                if (!isNaN(sps)) setSps(sps)
+                if (!isNaN(sps)) setFps(sps)
               }}
             />
           </label>
@@ -162,29 +188,18 @@ const App: Component = () => {
             Reset grid
           </button>
           <button
-            onClick={() => {
+            onClick={async () => {
               if (player()?.[0] !== 'Set' || goal()?.[0] !== 'Set') return
-              const steps = findPath((player() as ['Set', Pos])[1])
+              resetGrid()
+              setRunning(true)
+              setUsingGrid(true)
+              await findPath((player() as ['Set', Pos])[1])
+              setRunning(false)
+              setUsingGrid(false)
 
-              if (steps.length === 0) return
-
-              setGridUsed(true)
-
-              const walk = () => {
-                setRunning(true)
-                if (steps.length) {
-                  setTimeout(() => {
-                    const pos = steps.shift()!
-                    if (getTile(pos) !== Tile.Goal) setTile(pos, Tile.Path)
-                    else setTile(pos, Tile.ReachedGoal)
-                    walk()
-                  }, 1000 / sps())
-                } else setRunning(false)
-              }
-
-              walk()
+              if (steps().length === 0) return
             }}
-            disabled={isRunning() || player()[0] !== 'Set' || goal()[0] !== 'Set' || isGridUsed()}
+            disabled={isRunning() || player()[0] !== 'Set' || goal()[0] !== 'Set' || isUsingGrid()}
           >
             Run
           </button>
@@ -208,10 +223,12 @@ const App: Component = () => {
             onMouseDown={e => {
               setPlayer('Holding')
 
+              const player = e.currentTarget
+
               const dragPlayer = (e: MouseEvent) => {
-                playerRef.style.position = 'absolute'
-                playerRef.style.left = `${e.clientX - playerRef.clientWidth / 2}px`
-                playerRef.style.top = `${e.clientY - playerRef.clientHeight / 2}px`
+                player.style.position = 'absolute'
+                player.style.left = `${e.clientX - player.clientWidth / 2}px`
+                player.style.top = `${e.clientY - player.clientHeight / 2}px`
               }
 
               dragPlayer(e)
@@ -222,9 +239,9 @@ const App: Component = () => {
                 e => {
                   document.removeEventListener('mousemove', dragPlayer)
 
-                  playerRef.style.position = 'static'
-                  playerRef.style.left = '0'
-                  playerRef.style.top = '0'
+                  player.style.position = 'static'
+                  player.style.left = '0'
+                  player.style.top = '0'
 
                   const { top, left, width, height } = gridRef!.getBoundingClientRect()
                   const { clientX: cx, clientY: cy } = e
@@ -248,7 +265,6 @@ const App: Component = () => {
                 'background-color': colors[Tile.Player],
                 'border-radius': '50%',
               }}
-              ref={el => (playerRef = el)}
             />
           </div>
         </Show>
@@ -269,10 +285,12 @@ const App: Component = () => {
             onMouseDown={e => {
               setGoal('Holding')
 
+              const goal = e.currentTarget
+
               const dragGoal = (e: MouseEvent) => {
-                goalRef.style.position = 'absolute'
-                goalRef.style.left = `${e.clientX - goalRef.clientWidth / 2}px`
-                goalRef.style.top = `${e.clientY - goalRef.clientHeight / 2}px`
+                goal.style.position = 'absolute'
+                goal.style.left = `${e.clientX - goal.clientWidth / 2}px`
+                goal.style.top = `${e.clientY - goal.clientHeight / 2}px`
               }
 
               dragGoal(e)
@@ -283,9 +301,9 @@ const App: Component = () => {
                 e => {
                   document.removeEventListener('mousemove', dragGoal)
 
-                  goalRef.style.position = 'static'
-                  goalRef.style.left = '0'
-                  goalRef.style.top = '0'
+                  goal.style.position = 'static'
+                  goal.style.left = '0'
+                  goal.style.top = '0'
 
                   const { top, left, width, height } = gridRef!.getBoundingClientRect()
                   const { clientX: cx, clientY: cy } = e
@@ -309,7 +327,6 @@ const App: Component = () => {
                 'background-color': colors[Tile.Goal],
                 'border-radius': '50%',
               }}
-              ref={el => (goalRef = el)}
             />
           </div>
         </Show>
